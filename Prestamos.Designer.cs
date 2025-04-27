a2095ba32ef0399247138eea8fafe79f70a8412a
Ebazet <ebazet@gmail.com>
2025-04-27 09:16:06 -0300

    se suben mas funcionalidades

--- Prestamos.Designer.cs
+++ Prestamos.Designer.cs
@@ -28,33 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmr_prestamos));
             this.lbl_saludo = new System.Windows.Forms.Label();
             this.btn_confirmarSolicitud = new System.Windows.Forms.Button();
             this.btn_volver = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbl_datosPersonales = new System.Windows.Forms.Label();
+            this.lbl_detaPrestamo = new System.Windows.Forms.Label();
+            this.gbx_datosPersonales = new System.Windows.Forms.GroupBox();
+            this.cbx_sexo = new System.Windows.Forms.ComboBox();
             this.cbx_origen = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.cbx_sexo = new System.Windows.Forms.ComboBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.cbx_cuotas = new System.Windows.Forms.ComboBox();
+            this.gbx_detaPrestamo = new System.Windows.Forms.GroupBox();
             this.txt_monto = new System.Windows.Forms.TextBox();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
+            this.cbx_cuotas = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.gbx_datosPersonales.SuspendLayout();
+            this.gbx_detaPrestamo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // lbl_saludo
             // 
             this.lbl_saludo.AutoSize = true;
             this.lbl_saludo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_saludo.Location = new System.Drawing.Point(27, 37);
+            this.lbl_saludo.Location = new System.Drawing.Point(14, 19);
+            this.lbl_saludo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_saludo.Name = "lbl_saludo";
-            this.lbl_saludo.Size = new System.Drawing.Size(228, 44);
+            this.lbl_saludo.Size = new System.Drawing.Size(115, 24);
             this.lbl_saludo.TabIndex = 0;
             this.lbl_saludo.Text = "Bienvenido, ";
             // 
@@ -62,9 +66,10 @@
             // 
             this.btn_confirmarSolicitud.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.btn_confirmarSolicitud.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.btn_confirmarSolicitud.Location = new System.Drawing.Point(345, 466);
+            this.btn_confirmarSolicitud.Location = new System.Drawing.Point(172, 242);
+            this.btn_confirmarSolicitud.Margin = new System.Windows.Forms.Padding(2);
             this.btn_confirmarSolicitud.Name = "btn_confirmarSolicitud";
-            this.btn_confirmarSolicitud.Size = new System.Drawing.Size(140, 85);
+            this.btn_confirmarSolicitud.Size = new System.Drawing.Size(70, 44);
             this.btn_confirmarSolicitud.TabIndex = 1;
             this.btn_confirmarSolicitud.Text = "Confirmar Solicitud";
             this.btn_confirmarSolicitud.UseVisualStyleBackColor = false;
@@ -74,156 +79,179 @@
             // 
             this.btn_volver.BackColor = System.Drawing.Color.OrangeRed;
             this.btn_volver.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.btn_volver.Location = new System.Drawing.Point(669, 526);
+            this.btn_volver.Location = new System.Drawing.Point(334, 274);
+            this.btn_volver.Margin = new System.Windows.Forms.Padding(2);
             this.btn_volver.Name = "btn_volver";
-            this.btn_volver.Size = new System.Drawing.Size(129, 72);
+            this.btn_volver.Size = new System.Drawing.Size(64, 37);
             this.btn_volver.TabIndex = 2;
             this.btn_volver.Text = "Volver";
             this.btn_volver.UseVisualStyleBackColor = false;
+            this.btn_volver.Click += new System.EventHandler(this.btn_volver_Click);
             // 
-            // label1
+            // lbl_datosPersonales
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 158);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(186, 25);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Datos personales:";
+            this.lbl_datosPersonales.AutoSize = true;
+            this.lbl_datosPersonales.Location = new System.Drawing.Point(18, 82);
+            this.lbl_datosPersonales.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_datosPersonales.Name = "lbl_datosPersonales";
+            this.lbl_datosPersonales.Size = new System.Drawing.Size(92, 13);
+            this.lbl_datosPersonales.TabIndex = 3;
+            this.lbl_datosPersonales.Text = "Datos personales:";
             // 
-            // label2
+            // lbl_detaPrestamo
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(465, 158);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(226, 25);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Detalles del préstamo:";
+            this.lbl_detaPrestamo.AutoSize = true;
+            this.lbl_detaPrestamo.Location = new System.Drawing.Point(232, 82);
+            this.lbl_detaPrestamo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_detaPrestamo.Name = "lbl_detaPrestamo";
+            this.lbl_detaPrestamo.Size = new System.Drawing.Size(111, 13);
+            this.lbl_detaPrestamo.TabIndex = 4;
+            this.lbl_detaPrestamo.Text = "Detalles del préstamo:";
             // 
-            // groupBox1
+            // gbx_datosPersonales
             // 
-            this.groupBox1.BackColor = System.Drawing.Color.Moccasin;
-            this.groupBox1.Controls.Add(this.cbx_sexo);
-            this.groupBox1.Controls.Add(this.cbx_origen);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Location = new System.Drawing.Point(40, 206);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(328, 242);
-            this.groupBox1.TabIndex = 5;
-            this.groupBox1.TabStop = false;
-            // 
-            // cbx_origen
-            // 
-            this.cbx_origen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbx_origen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.cbx_origen.FormattingEnabled = true;
-            this.cbx_origen.Location = new System.Drawing.Point(136, 143);
-            this.cbx_origen.Name = "cbx_origen";
-            this.cbx_origen.Size = new System.Drawing.Size(174, 33);
-            this.cbx_origen.TabIndex = 2;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(31, 151);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(82, 25);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Origen:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(31, 59);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 25);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Sexo:";
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.BackColor = System.Drawing.Color.Moccasin;
-            this.groupBox2.Controls.Add(this.txt_monto);
-            this.groupBox2.Controls.Add(this.cbx_cuotas);
-            this.groupBox2.Controls.Add(this.label6);
-            this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Location = new System.Drawing.Point(470, 206);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(328, 242);
-            this.groupBox2.TabIndex = 6;
-            this.groupBox2.TabStop = false;
+            this.gbx_datosPersonales.BackColor = System.Drawing.Color.Moccasin;
+            this.gbx_datosPersonales.Controls.Add(this.cbx_sexo);
+            this.gbx_datosPersonales.Controls.Add(this.cbx_origen);
+            this.gbx_datosPersonales.Controls.Add(this.label4);
+            this.gbx_datosPersonales.Controls.Add(this.label3);
+            this.errorProvider1.SetIconAlignment(this.gbx_datosPersonales, System.Windows.Forms.ErrorIconAlignment.MiddleLeft);
+            this.gbx_datosPersonales.Location = new System.Drawing.Point(20, 107);
+            this.gbx_datosPersonales.Margin = new System.Windows.Forms.Padding(2);
+            this.gbx_datosPersonales.Name = "gbx_datosPersonales";
+            this.gbx_datosPersonales.Padding = new System.Windows.Forms.Padding(2);
+            this.gbx_datosPersonales.Size = new System.Drawing.Size(164, 126);
+            this.gbx_datosPersonales.TabIndex = 5;
+            this.gbx_datosPersonales.TabStop = false;
             // 
             // cbx_sexo
             // 
             this.cbx_sexo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbx_sexo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.cbx_sexo.FormattingEnabled = true;
-            this.cbx_sexo.Location = new System.Drawing.Point(136, 59);
+            this.cbx_sexo.Location = new System.Drawing.Point(68, 31);
+            this.cbx_sexo.Margin = new System.Windows.Forms.Padding(2);
             this.cbx_sexo.Name = "cbx_sexo";
-            this.cbx_sexo.Size = new System.Drawing.Size(174, 33);
+            this.cbx_sexo.Size = new System.Drawing.Size(89, 21);
             this.cbx_sexo.TabIndex = 3;
             // 
-            // label5
+            // cbx_origen
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(34, 59);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(78, 25);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Monto:";
+            this.cbx_origen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbx_origen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbx_origen.FormattingEnabled = true;
+            this.cbx_origen.Location = new System.Drawing.Point(68, 74);
+            this.cbx_origen.Margin = new System.Windows.Forms.Padding(2);
+            this.cbx_origen.Name = "cbx_origen";
+            this.cbx_origen.Size = new System.Drawing.Size(89, 21);
+            this.cbx_origen.TabIndex = 2;
             // 
-            // label6
+            // label4
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(34, 151);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(86, 25);
-            this.label6.TabIndex = 1;
-            this.label6.Text = "Cuotas:";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 79);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Origen:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 31);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Sexo:";
+            // 
+            // gbx_detaPrestamo
+            // 
+            this.gbx_detaPrestamo.BackColor = System.Drawing.Color.Moccasin;
+            this.gbx_detaPrestamo.Controls.Add(this.txt_monto);
+            this.gbx_detaPrestamo.Controls.Add(this.cbx_cuotas);
+            this.gbx_detaPrestamo.Controls.Add(this.label6);
+            this.gbx_detaPrestamo.Controls.Add(this.label5);
+            this.gbx_detaPrestamo.Location = new System.Drawing.Point(235, 107);
+            this.gbx_detaPrestamo.Margin = new System.Windows.Forms.Padding(2);
+            this.gbx_detaPrestamo.Name = "gbx_detaPrestamo";
+            this.gbx_detaPrestamo.Padding = new System.Windows.Forms.Padding(2);
+            this.gbx_detaPrestamo.Size = new System.Drawing.Size(164, 126);
+            this.gbx_detaPrestamo.TabIndex = 6;
+            this.gbx_detaPrestamo.TabStop = false;
+            // 
+            // txt_monto
+            // 
+            this.txt_monto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_monto.Location = new System.Drawing.Point(68, 31);
+            this.txt_monto.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_monto.Name = "txt_monto";
+            this.txt_monto.Size = new System.Drawing.Size(88, 20);
+            this.txt_monto.TabIndex = 3;
             // 
             // cbx_cuotas
             // 
             this.cbx_cuotas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbx_cuotas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.cbx_cuotas.FormattingEnabled = true;
-            this.cbx_cuotas.Location = new System.Drawing.Point(135, 143);
+            this.cbx_cuotas.Location = new System.Drawing.Point(68, 74);
+            this.cbx_cuotas.Margin = new System.Windows.Forms.Padding(2);
             this.cbx_cuotas.Name = "cbx_cuotas";
-            this.cbx_cuotas.Size = new System.Drawing.Size(174, 33);
+            this.cbx_cuotas.Size = new System.Drawing.Size(89, 21);
             this.cbx_cuotas.TabIndex = 2;
             // 
-            // txt_monto
+            // label6
             // 
-            this.txt_monto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txt_monto.Location = new System.Drawing.Point(135, 59);
-            this.txt_monto.Name = "txt_monto";
-            this.txt_monto.Size = new System.Drawing.Size(174, 31);
-            this.txt_monto.TabIndex = 3;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 79);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Cuotas:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 31);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Monto:";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
             // 
             // fmr_prestamos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Cornsilk;
-            this.ClientSize = new System.Drawing.Size(832, 610);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(416, 317);
+            this.Controls.Add(this.gbx_detaPrestamo);
+            this.Controls.Add(this.gbx_datosPersonales);
+            this.Controls.Add(this.lbl_detaPrestamo);
+            this.Controls.Add(this.lbl_datosPersonales);
             this.Controls.Add(this.btn_volver);
             this.Controls.Add(this.btn_confirmarSolicitud);
             this.Controls.Add(this.lbl_saludo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "fmr_prestamos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Prestamos";
             this.Load += new System.EventHandler(this.fmr_prestamos_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
+            this.gbx_datosPersonales.ResumeLayout(false);
+            this.gbx_datosPersonales.PerformLayout();
+            this.gbx_detaPrestamo.ResumeLayout(false);
+            this.gbx_detaPrestamo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -234,17 +262,18 @@
         private System.Windows.Forms.Label lbl_saludo;
         private System.Windows.Forms.Button btn_confirmarSolicitud;
         private System.Windows.Forms.Button btn_volver;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbl_datosPersonales;
+        private System.Windows.Forms.Label lbl_detaPrestamo;
+        private System.Windows.Forms.GroupBox gbx_datosPersonales;
         private System.Windows.Forms.ComboBox cbx_origen;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox gbx_detaPrestamo;
         private System.Windows.Forms.ComboBox cbx_sexo;
         private System.Windows.Forms.ComboBox cbx_cuotas;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txt_monto;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
--- Prestamos.cs
+++ Prestamos.cs
@@ -37,7 +37,35 @@ namespace BancoPrestamoBasico
 
         private void btn_confirmarSolicitud_Click(object sender, EventArgs e)
         {
-
+            switch (validaciones())
+            {
+                case 0:
+                {
+                    errorProvider1.SetError(gbx_datosPersonales, "");
+                    errorProvider1.SetError(gbx_detaPrestamo, "");
+                    double interesMensual = calcularInteres();
+                    double montoPedido = double.Parse(txt_monto.Text);
+                    int cuotasPedidas = (int)cbx_cuotas.SelectedIndex;
+                    double interesesTotal = montoPedido * (interesMensual / 100) * cuotasPedidas;
+                    double montoPagar = montoPedido + interesesTotal;
+                    string mensaje = "Su préstamo por " + montoPedido + " en " + cuotasPedidas + " cuotas se consederá con un interés del " + interesMensual + "% mensual \nEl monto final ascienda a " + montoPagar;
+                    MessageBoxButtons  buttons = MessageBoxButtons.OK;
+                    MessageBox.Show(mensaje,"Cálculo de intereses",buttons);
+                    break;
+                }
+                case 1:
+                {
+                    errorProvider1.SetError(gbx_datosPersonales, "Completar todos los datos personales");
+                    errorProvider1.SetError(gbx_detaPrestamo, "");
+                    break;
+                }
+                case 2:
+                {
+                    errorProvider1.SetError(gbx_datosPersonales, "");
+                    errorProvider1.SetError(gbx_detaPrestamo, "Debe ingresar un monto númerico y una cantidad de cuotas");
+                    break;
+                }
+            }
         }
 
         private void fmr_prestamos_Load(object sender, EventArgs e)
@@ -56,7 +84,7 @@ namespace BancoPrestamoBasico
             }
         }
 
-          void popularSexo()
+        void popularSexo()
         {
             for (int i = 0; i < sexo.Length; i++)
             {
@@ -66,11 +94,55 @@ namespace BancoPrestamoBasico
 
         void popularCiudades()
         {
-            for (int i = 0; i < lugaresDisponibles.Length; i++) 
+            for (int i = 0; i < lugaresDisponibles.Length; i++)
             {
-               cbx_origen.Items.Add(lugaresDisponibles[i]);
+                cbx_origen.Items.Add(lugaresDisponibles[i]);
             }
 
         }
+
+        private void btn_volver_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        double calcularInteres()
+        {
+            int cuotasPedidas = (int)cbx_cuotas.SelectedItem;
+            string sexoSeleccionado = cbx_sexo.ToString().ToLower();
+            string origenSeleccionado = cbx_origen.ToString().ToLower();
+            double interes = interesesBase[cuotasPedidas];
+            if (new[] { "Femenino" }.Contains(sexoSeleccionado))
+            {
+                interes += 0.3;
+            }
+            if (new[] { "rivendell", "tirion", "valinor", "hobbiton", "bree", "gondor" }.Contains(origenSeleccionado))
+            {
+                interes -= 0.3;
+            }
+
+            return interes;
+        }
+
+        int validaciones()
+        {
+            if ((cbx_sexo.SelectedIndex <= -1) || (cbx_origen.SelectedIndex <= -1))
+            {
+                return 1;
+            }
+            else
+            {
+                if (!(txt_monto.Text.All(char.IsDigit)) || (txt_monto.Text = "") || (cbx_cuotas.SelectedIndex > -1))
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+            
+
     }
 }
